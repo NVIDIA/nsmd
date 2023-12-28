043a1a1d75e015fb1e44089ac34e2582e8db0124
Gilbert Chen <gilbertc@nvidia.com>
2023-12-28 01:22:12 -0800

    add mockup responder
    mockup responder implementation
    
    Fixes JIRA https://jirasw.nvidia.com/browse/DGXOPENBMC-8373
    
    Signed-off-by: Gilbert Chen <gilbertc@nvidia.com>

---
+++ mockupResponder/mockupResponder.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "base.h"
+
+#include <sdbusplus/asio/object_server.hpp>
+#include <sdeventplus/event.hpp>
+
+#include <optional>
+
+namespace MockupResponder
+{
+constexpr uint8_t MCTP_MSG_TYPE_VDM = 0x7e;
+constexpr uint8_t MCTP_MSG_EMU_PREFIX = 0xFF;
+// these are for use with the mctp-demux-daemon
+constexpr size_t mctpMaxMessageSize = 4096;
+
+constexpr char MCTP_SOCKET_PATH[] = "\0mctp-pcie-mux";
+
+struct HeaderType
+{
+    uint8_t eid;
+    uint8_t type;
+};
+
+class MockupResponder
+{
+  public:
+    MockupResponder(bool verbose, sdeventplus::Event& event) :
+        event(event), verbose(verbose)
+    {}
+
+    ~MockupResponder()
+    {}
+
+    int connectMockupEID(uint8_t eid);
+
+    std::optional<std::vector<uint8_t>>
+        processRxMsg(const std::vector<uint8_t>& request);
+
+    // type0 handlers
+    std::optional<std::vector<uint8_t>>
+        unsupportedCommandHandler(const nsm_msg* requestMsg, size_t requestLen);
+    std::optional<std::vector<uint8_t>> pingHandler(const nsm_msg* requestMsg,
+                                                    size_t requestLen);
+    std::optional<std::vector<uint8_t>>
+        getSupportNvidiaMessageTypesHandler(const nsm_msg* requestMsg,
+                                            size_t requestLen);
+    std::optional<std::vector<uint8_t>>
+        getSupportCommandCodeHandler(const nsm_msg* requestMsg,
+                                     size_t requestLen);
+    std::optional<std::vector<uint8_t>>
+        queryDeviceIdentificationHandler(const nsm_msg* requestMsg,
+                                         size_t requestLen);
+
+    // type3 handlers
+    std::optional<std::vector<uint8_t>>
+        getInventoryInformationHandler(const nsm_msg* requestMsg,
+                                       size_t requestLen);
+    std::vector<uint8_t> getProperty(uint32_t transfer_handle,
+                                     uint32_t& next_transfer_handle);
+    std::optional<std::vector<uint8_t>>
+        getTemperatureReadingHandler(const nsm_msg* requestMsg,
+                                     size_t requestLen);
+
+  private:
+    sdeventplus::Event& event;
+    bool verbose;
+};
+
+} // namespace MockupResponder
