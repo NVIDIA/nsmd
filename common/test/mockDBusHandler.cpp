c12cdd73d4a21539845fff4309a413f9bb4e9047
Paweł Iwaneczko <piwaneczko@nvidia.com>
2024-05-07 10:57:58 +0200

    Refactor SensorManager and DBusHandler usage
    This commit enhances the utilization of SensorManager and DBusHandler
    within the codebase to improve testability and maintainability. The
    changes include transitioning from direct references to SensorManager
    to using the SensorManager abstraction. This abstraction encapsulates
    only necessary functions, facilitating easier mocking during unit
    tests. Additionally, the constructor of utils::DBusHandler has been
    replaced with a function, utils::DBusHandler(), returning an
    IDBusHandler& object for seamless mocking during unit testing.
    Furthermore, the deviceManager.cpp file has been refactored to enhance
    clarity and structure.
    
    Fixes JIRA https://

---
+++ common/test/mockDBusHandler.cpp
@@ -0,0 +1,38 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-2024 NVIDIA CORPORATION &
+ * AFFILIATES. All rights reserved. SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mockDBusHandler.hpp"
+
+namespace utils
+{
+IDBusHandler& DBusHandler()
+{
+    return MockDBusHandler::instance();
+}
+
+PropertyValue DBusTest::get(const PropertyValuesCollection& properties,
+                            const std::string& name)
+{
+    return std::find_if(
+               properties.begin(), properties.end(),
+               [&name](const std::pair<std::string, PropertyValue>& pair) {
+                   return pair.first == name;
+               })
+        ->second;
+}
+
+} // namespace utils
---
+++ common/test/mockDBusHandler.hpp
@@ -0,0 +1,74 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-2024 NVIDIA CORPORATION &
+ * AFFILIATES. All rights reserved. SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "utils.hpp"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace utils
+{
+
+class MockDBusHandler : public IDBusHandler
+{
+    MockDBusHandler() = default;
+
+  public:
+    static MockDBusHandler& instance()
+    {
+        static MockDBusHandler mockDBusHandler;
+        return mockDBusHandler;
+    }
+
+    MOCK_METHOD(std::string, getService,
+                (const char* path, const char* interface), (const, override));
+    MOCK_METHOD(GetSubTreeResponse, getSubtree,
+                (const std::string& path, int depth,
+                 const std::vector<std::string>& ifaceList),
+                (const, override));
+
+    MOCK_METHOD(void, setDbusProperty,
+                (const DBusMapping& dBusMap, const PropertyValue& value),
+                (const, override));
+
+    MOCK_METHOD(PropertyValue, getDbusPropertyVariant,
+                (const char* objPath, const char* dbusProp,
+                 const char* dbusInterface),
+                (const, override));
+    MOCK_METHOD(PropertyValuesCollection, getDbusProperties,
+                (const char* objPath, const char* dbusInterface),
+                (const, override));
+};
+
+class DBusTest
+{
+  protected:
+    MockDBusHandler& mockDBus = MockDBusHandler::instance();
+
+    static PropertyValue get(const PropertyValuesCollection& properties,
+                             const std::string& name);
+    template <typename T>
+    static T get(const PropertyValuesCollection& properties,
+                 const std::string& name)
+    {
+        return std::get<T>(get(properties, name));
+    }
+};
+
+} // namespace utils
