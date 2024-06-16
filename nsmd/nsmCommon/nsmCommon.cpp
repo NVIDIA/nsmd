5ce7a65ff665c18fabf29109d8e3188e433de4a6
Ayush Kumar Thakur <ayushkumart@nvidia.com>
2024-06-16 13:12:31 +0530

    Fixes nvbug https://nvbugspro.nvidia.com/bug/4652180 signed-off-by: <ayushkumart@nvidia.com>

--- nsmd/nsmCommon/nsmCommon.cpp
+++ nsmd/nsmCommon/nsmCommon.cpp
@@ -69,4 +69,110 @@ uint8_t NsmMemoryCapacity::handleResponseMsg(const struct nsm_msg* responseMsg,
     return cc;
 }
 
+NsmTotalMemory::NsmTotalMemory(const std::string& name,
+                               const std::string& type) :
+    NsmMemoryCapacity(name, type)
+{
+    lg2::info("NsmTotalMemory : create sensor:{NAME}", "NAME", name.c_str());
+}
+
+void NsmTotalMemory::updateReading(uint32_t* maximumMemoryCapacity)
+{
+    if (maximumMemoryCapacity)
+    {
+        totalMemoryCapacity = new uint32_t(*maximumMemoryCapacity);
+    }
+    else
+    {
+        totalMemoryCapacity = nullptr;
+    }
+}
+
+const uint32_t* NsmTotalMemory::getReading()
+{
+    return totalMemoryCapacity;
+}
+
+NsmMemoryCapacityUtil::NsmMemoryCapacityUtil(
+    sdbusplus::bus::bus& bus, const std::string& name, const std::string& type,
+    std::string& inventoryObjPath,
+    std::shared_ptr<NsmTotalMemory> totalMemory) :
+    NsmSensor(name, type),
+    totalMemory(totalMemory)
+
+{
+    lg2::info("NsmMemoryCapacityUtil: create sensor:{NAME}", "NAME",
+              name.c_str());
+    dimmMemoryMetricsIntf =
+        std::make_unique<DimmMemoryMetricsIntf>(bus, inventoryObjPath.c_str());
+}
+
+void NsmMemoryCapacityUtil::updateReading(
+    const struct nsm_memory_capacity_utilization& data)
+{
+    const uint32_t* totalMemoryCapacity = totalMemory->getReading();
+    if (totalMemoryCapacity == NULL)
+    {
+        lg2::error(
+            "NsmMemoryCapacityUtil::updateReading unable to fetch total Memory Capacity data");
+        return;
+    }
+    else if ((*totalMemoryCapacity) == 0)
+    {
+        lg2::error(
+            "NsmMemoryCapacityUtil::updateReading total Memory Capacity value is {A}",
+            "A", (*totalMemoryCapacity));
+        return;
+    }
+
+    uint8_t usedMemoryPercent = (data.used_memory + data.reserved_memory) *
+                                100 / (*totalMemoryCapacity);
+    dimmMemoryMetricsIntf->capacityUtilizationPercent(usedMemoryPercent);
+}
+
+std::optional<std::vector<uint8_t>>
+    NsmMemoryCapacityUtil::genRequestMsg(eid_t eid, uint8_t instanceId)
+{
+    std::vector<uint8_t> request(sizeof(nsm_msg_hdr) + sizeof(nsm_common_req));
+    auto requestPtr = reinterpret_cast<struct nsm_msg*>(request.data());
+    auto rc = encode_get_memory_capacity_util_req(instanceId, requestPtr);
+    if (rc != NSM_SW_SUCCESS)
+    {
+        lg2::error("encode_get_memory_capacity_util_req failed. "
+                   "eid={EID} rc={RC}",
+                   "EID", eid, "RC", rc);
+        return std::nullopt;
+    }
+
+    return request;
+}
+
+uint8_t
+    NsmMemoryCapacityUtil::handleResponseMsg(const struct nsm_msg* responseMsg,
+                                             size_t responseLen)
+{
+    uint8_t cc = NSM_ERROR;
+    struct nsm_memory_capacity_utilization data;
+    uint16_t data_size;
+    uint16_t reason_code = ERR_NULL;
+
+    auto rc = decode_get_memory_capacity_util_resp(
+        responseMsg, responseLen, &cc, &data_size, &reason_code, &data);
+
+    if (cc == NSM_SUCCESS && rc == NSM_SW_SUCCESS)
+    {
+        updateReading(data);
+    }
+    else
+    {
+        lg2::error(
+            "handleResponseMsg: decode_get_memory_capacity_util_resp "
+            "sensor={NAME} with reasonCode={REASONCODE}, cc={CC} and rc={RC}",
+            "NAME", getName(), "REASONCODE", reason_code, "CC", cc, "RC", rc);
+        return NSM_SW_ERROR_COMMAND_FAIL;
+    }
+
+    return cc;
+}
+
 } // namespace nsm
