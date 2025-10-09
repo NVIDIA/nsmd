64aa634c5e0de7336b7628bdc5538fd5e8c02915
HARSH VERMA <harshv@nvidia.com>
2025-10-09 17:51:03 +0530

    Update PCIe Device Type macro from string to enum
    Fixes NVBug https://nvbugspro.nvidia.com/bug/5541247

--- common/utils.hpp
+++ common/utils.hpp
@@ -76,7 +76,8 @@ using PropertyValuesCollection =
 #define MANUFACTURER_NVIDIA "NVIDIA"
 #define HEALTH_TYPE_OK                                                         \
     "xyz.openbmc_project.State.Decorator.Health.HealthType.OK"
-#define PCIE_DEVICE_TYPE_SINGLE_FUNCTION "SingleFunction"
+#define PCIE_DEVICE_TYPE_SINGLE_FUNCTION                                       \
+    "xyz.openbmc_project.Inventory.Item.PCIeDevice.SingleFunction"
 
 // Largest safe integer for double precision (2^53 - 1)
 const uint64_t MAX_SAFE_INTEGER_IN_DOUBLE = (1ULL << 53) - 1;
